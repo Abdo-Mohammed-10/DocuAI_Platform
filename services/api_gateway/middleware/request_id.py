bf6f2b78ff9fb17227cb91c22fffc499bb5580cc
Abdo-Mohammed-10 <abdooomo33@gmail.com>
2026-06-08 21:14:08 +0300

    feat(tracing): implement request ID middleware

--- services/api_gateway/middleware/request_id.py
+++ services/api_gateway/middleware/request_id.py
@@ -6,9 +6,15 @@ from shared.tracing import generate_request_id
 
 class RequestIDMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        request_id = generate_request_id()
+
+        request_id = request.headers.get(
+            "X-Request-ID"
+        ) or generate_request_id()
+
+        request.state.request_id = request_id
 
         response = await call_next(request)
 
         response.headers["X-Request-ID"] = request_id
+
         return response
